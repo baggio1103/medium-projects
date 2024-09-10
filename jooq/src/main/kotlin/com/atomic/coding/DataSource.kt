d93dbb96998325d1174cb773c4c6825a0bea2c21
baggio1103 <valijanov1103@mail.ru>
2024-09-10 22:26:00 +0300

    Extract hikariDataSource to separate file

--- jooq/src/main/kotlin/com/atomic/coding/DataSource.kt
+++ jooq/src/main/kotlin/com/atomic/coding/DataSource.kt
@@ -1,10 +1,10 @@
 package com.atomic.coding
 
-//import com.zaxxer.hikari.HikariDataSource
-//
-//fun hikariDataSource(): HikariDataSource = HikariDataSource()
-//    .apply {
-//        jdbcUrl = "jdbc:postgresql://localhost:5432/posts"
-//        username = "posts"
-//        password = "posts-password"
-//    }
+import com.zaxxer.hikari.HikariDataSource
+
+fun hikariDataSource(): HikariDataSource = HikariDataSource()
+    .apply {
+        jdbcUrl = "jdbc:postgresql://localhost:5432/posts"
+        username = "posts"
+        password = "posts-password"
+    }
--- jooq/src/main/kotlin/com/atomic/coding/Main.kt
+++ jooq/src/main/kotlin/com/atomic/coding/Main.kt
@@ -1,7 +1,6 @@
 package com.atomic.coding
 
 import com.atomicCoding.generated.public_.tables.Posts
-import com.zaxxer.hikari.HikariDataSource
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import org.slf4j.LoggerFactory
@@ -13,10 +12,3 @@ fun main() {
     val println = { value: Any -> logger.info(value.toString()) }
     dsl.selectFrom(Posts.POSTS).fetch().forEach(println)
 }
-
-fun hikariDataSource(): HikariDataSource = HikariDataSource()
-    .apply {
-        jdbcUrl = "jdbc:postgresql://localhost:5432/posts"
-        username = "posts"
-        password = "posts-password"
-    }
