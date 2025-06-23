7c74d3cf451cae0e2fcf2aaad6b471a2e6bca8eb
baggio1103 <valijanov1103@mail.ru>
2025-06-23 10:56:46 +0300

    Refactoring

--- jooq/src/main/kotlin/com/atomic/coding/Main.kt
+++ jooq/src/main/kotlin/com/atomic/coding/Main.kt
@@ -1,14 +1,39 @@
 package com.atomic.coding
 
-import com.atomicCoding.generated.public_.tables.Authors
+import com.atomic.coding.Application.CUSTOMER_NAME
+import com.atomic.coding.Application.logger
+import com.atomic.coding.Application.orderItems
+import com.atomic.coding.Application.orderService
+import com.atomic.coding.domain.OrderItem
+import com.atomic.coding.repository.ItemRepository
+import com.atomic.coding.repository.OrderRepository
+import com.atomic.coding.service.OrderService
+import com.atomic.coding.service.TransactionManager
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+object Application {
+    val logger: Logger = LoggerFactory.getLogger("Application")
+    private val dsl = DSL.using(
+        hikariDataSource(),
+        SQLDialect.POSTGRES
+    )
+    val orderService = OrderService(
+        orderRepository = OrderRepository(dsl),
+        itemRepository = ItemRepository(dsl),
+        transactionManager = TransactionManager(dsl)
+    )
+
+    val orderItems = listOf(
+        OrderItem(1, 10),
+        OrderItem(2, 2)
+    )
+    const val CUSTOMER_NAME = "Bruce"
+}
+
 fun main() {
-    val logger = LoggerFactory.getLogger("Jooq")
-    val dataSource = hikariDataSource()
-    val dsl = DSL.using(dataSource, SQLDialect.POSTGRES)
-    val println = { value: Any -> logger.info(value.toString()) }
-    dsl.selectFrom(Authors.AUTHORS).forEach(println)
+    val orders = orderService.order(customerName = CUSTOMER_NAME, orderItems = orderItems)
+    logger.info("Orders: $orders")
 }
